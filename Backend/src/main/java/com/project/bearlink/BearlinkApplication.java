a8bae300008404c7bd0973c95e818ed3fae375c5
asdf-qwe <fism@naver.com>
2025-05-17 15:07:54 +0900

    WIP

---
+++ Backend/src/main/java/com/project/bearlink/BearlinkApplication.java
@@ -0,0 +1,13 @@
+package com.project.bearlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearlinkApplication.class, args);
+	}
+
+}
