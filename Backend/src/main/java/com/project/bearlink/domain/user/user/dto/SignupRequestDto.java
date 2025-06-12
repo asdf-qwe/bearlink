182fae441e04fe1701196585ec93f6b95131b6ac
asdf-qwe <fism@naver.com>
2025-06-13 06:15:29 +0900

    WIP

--- Backend/src/main/java/com/project/bearlink/domain/user/user/dto/SignupRequestDto.java
+++ Backend/src/main/java/com/project/bearlink/domain/user/user/dto/SignupRequestDto.java
@@ -7,16 +7,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignupRequestDto {
-    @NotBlank(message = "아이디는 필수 입력값입니다. 이메일도 가능합니다.")
+    @NotBlank(message = "아이디는 필수 입력값입니다.")
     private String loginId;
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    private String email;
+
     @NotBlank(message = "역할은 필수 입력값입니다. (예: ARTIST, STAFF)")
     private String role;
 
     private String nickname;
-    private String email;
+
     private String imageUrl;
 }
